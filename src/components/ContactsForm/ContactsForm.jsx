8e4dc92a0c2ff381e54aadd70c7900b66ec18bc8
funz1k <92177445+funz1k@users.noreply.github.com>
2022-07-18 17:24:37 +0300

    task-02 step 1 & 2

---
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -0,0 +1,58 @@
+import { Component } from "react";
+
+export class ContactsForm extends Component {
+    state = {
+        name: '',
+        number: '',
+        id: '',
+    }
+
+    onInputChange = e => {
+        const { name, value } = e.currentTarget;
+
+        this.setState({ [name]: value });
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        this.props.onSubmit(this.state.name, this.state.number);
+        this.reset();
+    }
+
+    reset = () => {
+        this.setState({ name: '', number: '' });
+    };
+
+    render() {
+        const { number, name } = this.state;
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <label>Name
+                    <input
+                        type="text"
+                        name="name"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                        value={name}
+                        onChange={this.onInputChange}
+                    />
+                </label>
+                <label> Number
+                    <input
+                        type="tel"
+                        name="number"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                        value={number}
+                        onChange={this.onInputChange}
+                    />
+                </label>
+                <button type='submit'>Add contact</button>
+            </form>
+        )
+    }
+}
